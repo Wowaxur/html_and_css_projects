16c281e60a106c11c0d0ed3e3b846a8d1cf31f5f
Wowaxur <wowaxur@yandex.ru>
2024-06-02 03:12:36 +0300

    add junior Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import ProfileCard from "./pages/profileCard/ProfileCard";
+import Navbar from "./pages/navbar/Navbar";
+import MainPage from "./pages/mainPage/mainPage";
+import CitySkyline from "./pages/cityCss/cityCss";
+import Junior from "./pages/Junior/Junior";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar/>
+        <div className={"content"}>
+        <Routes>
+            <Route path="/" element={<MainPage/>}></Route>
+            <Route path="profileCard" element={<ProfileCard/>}></Route>
+            <Route path="CitySkyline" element={<CitySkyline/>}></Route>
+            <Route path="junior/*" element={<Junior />} />
+        </Routes>
+        </div>
     </div>
   );
 }
